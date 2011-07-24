66fe187899f028acb0c81f8ca0e04e3e16a5aa39
mgiagnoni <gimassimo@gmail.com>
2011-07-24 02:13:21 +0200

    Enabled, configured LyraContentBundle

--- app/autoload.php
+++ app/autoload.php
@@ -15,6 +15,7 @@ $loader->registerNamespaces(array(
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
     'Gedmo'            => __DIR__.'/../vendor/gedmo-doctrine-extensions/lib',
+    'Lyra'             => __DIR__.'/../vendor/bundles',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
